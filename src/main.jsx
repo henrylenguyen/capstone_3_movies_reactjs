f6339040b133899c6a6ca577d67fcb63f07a5d89
Le Nguyen Phuong Thai <thailnp133@gmail.com>
2023-03-14 00:35:28 +0700

    Cài thư viện

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import store from "config/configureStore";
 import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
